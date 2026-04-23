592bb778f59d48cae4a1618304b1a152e515c24f
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-23 11:29:27 +0400

    add Task 2 - create a stack and do many operations like add, search and check, also iterate the stack from top to down and the opposit

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -2,6 +2,22 @@ import java.util.Stack;
 
 public class EmployeeStackSearch {
     public static void main(String[] args){
-
+        Stack<String> employee = new Stack<>();
+        employee.push("Wejdan"); // add elements to the stack
+        employee.push("Hoor");
+        employee.push("Omar");
+        employee.push("Sara");
+        employee.push("Muna");
+        employee.push("Said");
+        System.out.println("the current stack is: "+employee); //display the stack
+        System.out.println("search for employee Sara: "+employee.search("Sara")); //search for specific employee
+        System.out.println("search for employee Said: " + employee.search("Said"));
+        System.out.println("check if employee Noor is exist or not: "+employee.contains("Noor")); // check if the stack contain this employee or not
+        for(int i= employee.size()-1; i>=0; i--){ // iterate the stack from top to down
+            System.out.println(employee.get(i));
+        }
+        for(int i= 0; i<employee.size(); i++){ // iterate the stack from down to top
+            System.out.println(employee.get(i));
+        }
     }
 }
