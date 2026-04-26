9a1d7e66410c8feabf1a4118794efc1a4a02129e
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-26 13:41:04 +0400

    add Task 9 - implement redo and undo function using 2 stacks

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -1,13 +1,53 @@
 import java.util.Stack;
 
 public class TextEditorWithUndoRedo {
-    public static void main(String[] args){}
-    public static String type(String text){}
-    public static String undo(){
-        Stack<String> text=new Stack<>();
+    static  Stack<String> undo = new Stack<>();
+    static   Stack<String> redo = new Stack<>();
+    static String currentState ="";
+    static int max=10;
+    public static void main(String[] args){
+        type("Java ");
+        display();
+        type(" Programming ");
+        display();
+        type("!");
+        display();
+        undo();
+        display();
+        undo();
+        display();
+        redo();
+        display();
+        type(" Java");
+        display();
+        undo();
+        display();
     }
-    public static String redo(){
-        Stack<String> text=new Stack<>();
+    public static void type(String text){
+        if (undo.size()>=max){
+            System.out.println("reach undo limited");
+            undo.remove(0);
+        }
+        undo.push(currentState);
+        currentState+=text;
+        redo.clear();
+        System.out.println("typed :" +text );
     }
-    public static String display(){}
+    public static void undo(){
+        if (!(undo.isEmpty())) {
+            redo.push(currentState);
+            currentState = undo.pop();
+        }
+    }
+    public static void redo(){
+        if (!(redo.isEmpty())){
+            undo.push(currentState);
+            currentState = redo.pop();
+        }
+    }
+    public static void display(){
+        System.out.println("current text : " + currentState);
+        System.out.println("undo stack   : " + undo);
+        System.out.println("redo stack   : " + redo);
+    }
 }
