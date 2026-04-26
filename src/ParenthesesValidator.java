0bab975bd4794daaf1ea0d08eb74f5f05a773917
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-26 09:47:15 +0400

    add Task 4 - checking the balancing of paranthese

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -2,19 +2,21 @@ import java.util.*;
 
 public class ParenthesesValidator {
     public static void main(String[] args){
-        Stack<String> parentheses=new Stack<>();
-        parentheses.push("()");
+        Stack<String> parentheses=new Stack<>(); // creating stack
+        parentheses.push("()"); // add elements to the stack
         parentheses.push("[]");
         parentheses.push("{}");
         parentheses.push("{)");
         parentheses.push("[}");
-        for(String item: parentheses){
+        for(String item: parentheses){ // enter the stack to check the validation of balancing
             System.out.println(isBalanced(item));
         }
     }
-    public static boolean  isBalanced(String expression){
+    public static boolean  isBalanced(String expression){ // method for checking the balancing of parentheses
         if(expression=="[]" || expression=="()" || expression=="{}"){
             return true;
+        }else if(expression.isEmpty()){
+            return false;
         }
         return false;
     }
