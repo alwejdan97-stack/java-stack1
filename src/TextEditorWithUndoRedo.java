0fa2cc620733cad9795a393c3b1fbbc809e59b5e
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-26 12:33:08 +0400

    add Task 8 - create another methods for the original text, undo, redo, and display the final result

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -1,3 +1,13 @@
+import java.util.Stack;
+
 public class TextEditorWithUndoRedo {
     public static void main(String[] args){}
+    public static String type(String text){}
+    public static String undo(){
+        Stack<String> text=new Stack<>();
+    }
+    public static String redo(){
+        Stack<String> text=new Stack<>();
+    }
+    public static String display(){}
 }
