ed14ffa04660e6dac68fca40bc2cdb6baebf0c6f
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-27 14:57:09 +0400

    add Task 16 - Sort a Stack Using Another Stack

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -2,9 +2,17 @@ import java.util.Stack;
 
 public class StackSorter {
     public static void main(String[] args){}
-    public static void sortStack(Stack<Integer> stack){}
-    public static void sortStackRecursive(Stack<Integer> stack){}
-    public static void insertInSortedOrder(Stack<Integer> stack, int value){}
-    public static void displayStack(Stack<Integer> stack, String name){}
+    public static void sortStack(Stack<Integer> stack){//sort stack using another stack
+
+    }
+    public static void sortStackRecursive(Stack<Integer> stack){ //recursive sorting approach
+
+    }
+    public static void insertInSortedOrder(Stack<Integer> stack, int value){ //hHelper for recursive sort
+
+    }
+    public static void displayStack(Stack<Integer> stack, String name){ //display stack contents
+
+    }
 
 }
