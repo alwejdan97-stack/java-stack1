afe1366ffe9f04a4f693b114830c510e95ec23a5
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-27 16:59:47 +0400

    add Task 17 - Evaluate Infix Expression Directly Using Two Stacks

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -1,18 +1,79 @@
+import java.util.Stack;
+
 public class InfixExpressionEvaluator {
-    public static void main(String[] args){}
-    public static void evaluate(String expression){ //Main evaluation method
-
+    public static void main(String[] args){
+        String[] expression={"3+4*2","(1+2)*3","10+5*6","100*(2+12)","8+2*5-3","(7+3)*(5-2)","20/5+6*3", "2^3+4", "10%3+6", "5+6*(2+3)", "50/(5+5)", "9-3*2"};
+        for(String str:expression){
+            System.out.println("expression: "+str);
+            System.out.println("result: "+evaluate(str));
+        }
     }
-    public static void applyOperation(double a, double b, char operator){//Perform arithmetic
-
+    public static double evaluate(String expression) { //Main evaluation method
+        Stack<String> operand = new Stack<>();
+        Stack<String> operator = new Stack<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char chr = expression.charAt(i);
+            if (chr == ' ') {
+                continue;
+            }
+            if(isNumber(chr)) {
+                StringBuilder number = new StringBuilder();
+                if (i < expression.length() && isNumber(expression.charAt(i))) {
+                    number.append(chr);
+                }
+                //operand.push(Double.parseDouble(number.toString()));
+            }
+            if(chr=='('){
+                operator.push(chr);
+            }else if(chr==')'){
+                while(!(operator.isEmpty())&& operator.peek()!='('){
+                    operator.pop();
+                }
+            }else if(isOperator(chr)){
+                while(!(operator.isEmpty())&&operator.peek()!='(' && getPrecedence(operator.peek())>=getPrecedence(chr)){}
+                    operator.push(chr);
+            }
+            while(!(operator.isEmpty())){
+                operand.pop();
+            }
+        }
     }
-    public static void getPrecedence(char operator){ //Get operator precedence
-
+    public static double applyOperation(double a, double b, char operator){//Perform arithmetic
+        if(operator=='+'){
+            return a+b;
+        }else if(operator=='-'){
+            return a-b;
+        }else if(operator=='*'){
+            return a*b;
+        }else if(operator=='/'){
+            return a/b;
+        }else if(operator=='%'){
+            return a%b;
+        }else if(operator=='^'){
+            return Math.pow(a,b);
+        }
+        return 0;
     }
-    public static void isOperator(char ch){ //Check if character is an operator
-
+    public static int getPrecedence(char operator){ //Get operator precedence
+        if(operator=='^'){
+            return 3;
+        }else if(operator=='*' || operator=='/' || operator=='%'){
+            return 2;
+        }else if(operator=='+' || operator=='-'){
+            return 1;
+        }
+        return 0;
     }
-    public static void isNumber(char ch){ //Check if character is a digit or decimal point
-
+    public static boolean isOperator(char ch){ //Check if character is an operator
+        if(ch=='/' || ch=='*' || ch=='-' || ch=='+' || ch=='%' || ch=='^'){
+            return true;
+        }
+        return false;
+    }
+    public static boolean isNumber(char ch){ //Check if character is a digit or decimal point
+        if(Character.isDigit(ch) || ch=='.'){
+            return true;
+        }
+        return false;
     }
 }
