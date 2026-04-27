a1ce09eb49f19606704f5d5d04f9a3e24c498015
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-27 09:38:56 +0400

    add Task 13 - Convert Infix to Postfix and Evaluate Using Java Stack

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -1,3 +1,8 @@
 public class InfixToPostfixConverter {
     public static void main(String[] args){}
+    public static void infixToPostfix(String infix){}
+    public static void evaluatePostfix(String postfix){}
+    public static void getPrecedence(char operator){}
+    public static void isOperator(char ch){}
+    public static void applyOperation(int a, int b, char operator){}
 }
