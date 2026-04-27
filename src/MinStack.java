5549de20b5321efdaef723f25c34b91991eb0ca1
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-27 13:56:08 +0400

    add Task 15 - Implement a Stack that Supports Get Minimum in O(1) Time

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,8 +1,44 @@
+import java.util.Stack;
+
 public class MinStack {
-    public static void main(String[] args){}
-    public void push(int value){}
-    public void pop(){}
-    public void peek(){}
-    public void getMin(){}
-    public void display(){}
+    Stack<Integer> mainStack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+    public static void main(String[] args){
+        MinStack min=new MinStack();
+        min.push(5);
+        min.push(4);
+        min.push(10);
+        min.push(8);
+        min.pop();
+        min.pop();
+        min.peek();
+        min.getMin();
+        min.push(25);
+        min.display();
+    }
+    public void push(int value){
+        mainStack.push(value);
+        if(minStack.isEmpty() || value <= minStack.peek()){
+            minStack.push(value);
+        }
+        System.out.println("push: "+ value);
+    }
+    public void pop(){
+        int remove=mainStack.pop();
+        if(remove==minStack.peek()){
+            minStack.pop();
+        }
+        System.out.println("pop: "+remove);
+    }
+    public void peek(){
+        System.out.println("peek: "+mainStack.peek());
+    }
+    public Integer getMin(){
+        return minStack.peek();
+    }
+    public void display(){
+        System.out.println("MainStack :"+mainStack);
+        System.out.println("MinStack :"+minStack);
+        System.out.println("current Min: "+ getMin());
+    }
 }
