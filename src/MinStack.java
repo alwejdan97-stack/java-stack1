3dd35c8f9085ab0dcd07d789e0c86a37f129d245
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-27 13:22:56 +0400

    add Task 15 - Implement a Stack that Supports Get Minimum in O(1) Time

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,3 +1,8 @@
 public class MinStack {
     public static void main(String[] args){}
+    public void push(int value){}
+    public void pop(){}
+    public void peek(){}
+    public void getMin(){}
+    public void display(){}
 }
