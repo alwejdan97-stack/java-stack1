902f4e92ea00c64b82814f2971f70b1bbdaa7280
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-27 11:45:01 +0400

    add Task 13 - creating NextGreaterElementFinder class, main method and demanded methods

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -1,3 +1,10 @@
+import java.util.Stack;
+
 public class NextGreaterElementFinder {
+    Stack<Integer> temp=new Stack<>();
     public static void main(String[] args){}
+    public static int findNextGreaterElement(int[] array){}
+    public static int findNextGreaterElementCircular(int[] arr){}
+    public static int findNextSmallerElement(int[] arr){}
+    public static int displayResults(int[] arr, int[] result){}
 }
