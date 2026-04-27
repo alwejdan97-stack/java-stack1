5a56171b00263716719d67fe756d7ca4482e6196
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-27 12:42:03 +0400

    add Task 14 - creating NextGreaterElementFinder class, main method and demanded methods

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -1,3 +1,9 @@
 public class QueueUsingTwoStacks {
     public static void main(String[] args){}
+    public static void enqueue(T element){}
+    public static void dequeue(){}
+    public static void peek(){}
+    public static void isEmpty(){}
+    public static void size(){}
+
 }
