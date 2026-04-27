4afa4fb51ee7cb512d21a251f91c1500598c1fad
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-27 14:53:09 +0400

    add Task 16 - Sort a Stack Using Another Stack

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -1,4 +1,10 @@
+import java.util.Stack;
+
 public class StackSorter {
     public static void main(String[] args){}
+    public static void sortStack(Stack<Integer> stack){}
+    public static void sortStackRecursive(Stack<Integer> stack){}
+    public static void insertInSortedOrder(Stack<Integer> stack, int value){}
+    public static void displayStack(Stack<Integer> stack, String name){}
 
 }
