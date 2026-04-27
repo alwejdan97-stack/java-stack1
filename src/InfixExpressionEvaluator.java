76fc05654748fb20669ca1de80de005d033c8f27
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-27 15:25:10 +0400

    add Task 17 - Evaluate Infix Expression Directly Using Two Stacks

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -1,8 +1,18 @@
 public class InfixExpressionEvaluator {
     public static void main(String[] args){}
-    public static void evaluate(String expression){}
-    public static void applyOperation(double a, double b, char operator){}
-    public static void getPrecedence(char operator){}
-    public static void isOperator(char ch){}
-    public static void isNumber(char ch){}
+    public static void evaluate(String expression){ //Main evaluation method
+
+    }
+    public static void applyOperation(double a, double b, char operator){//Perform arithmetic
+
+    }
+    public static void getPrecedence(char operator){ //Get operator precedence
+
+    }
+    public static void isOperator(char ch){ //Check if character is an operator
+
+    }
+    public static void isNumber(char ch){ //Check if character is a digit or decimal point
+
+    }
 }
