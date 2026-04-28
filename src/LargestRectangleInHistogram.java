42ceab2c3a3e4753c48bd3b07cf18a36d3a2e408
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-28 09:50:22 +0400

    add Task 18 - Find Largest Rectangle in Histogram Using Stack

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -5,12 +5,12 @@ public class LargestRectangleInHistogram {
     public static void main(String[] args){
         int[][] test={{2,1,5,6,2,3},{6,2,5,4,5,1,6},{5,4,3,2,1},{1,2,3,4,5},{7},{}};
         for(int[] heigh:test){
-            //System.out.println(displayHistogram(heigh));
             int maxArea=findLargestRectangle(heigh);
             displayResult(heigh,maxArea);
         }
     }
     public static int findLargestRectangle(int[] heights){
+        stack.clear();
         if(heights==null || heights.length==0){
             return 0;
         }
@@ -22,19 +22,20 @@ public class LargestRectangleInHistogram {
             }else{
                 int top=stack.pop();
                 System.out.println("the top element is: "+top);
-                //int width=stack.isEmpty()?i:i-stack.peek()-1;
-                int width=i-stack.peek()-1;
+                int width=stack.isEmpty()?i:i-stack.peek()-1;
+                //int width=i-stack.peek()-1;
                 int area=heights[top]*width;
                 System.out.println("the area is: "+heights[top] +" * "+width+ " = "+area);
-                maxArea=maxArea+Math.max(maxArea,area);
+                maxArea=Math.max(maxArea,area);
             }
             while(!(stack.isEmpty())){
                 int top=stack.pop();
                 System.out.println("the top element is: "+top);
-                int width=i-stack.peek()-1;
+                int width=stack.isEmpty()?i:i-stack.peek()-1;
+                //int width=i-stack.peek()-1;
                 int area=heights[top]*width;
                 System.out.println("the area is: "+heights[top] +" * "+width+ " = "+area);
-                maxArea=maxArea+Math.max(maxArea,area);
+                maxArea=Math.max(maxArea,area);
             }
         }
         return maxArea;
@@ -48,23 +49,30 @@ public class LargestRectangleInHistogram {
     public static void displayHistogram(int[] heights){
         //using ASCII to display histogram
         if(heights.length==0){
-            System.out.println("heighest is empty");
+            System.out.println("heigh is empty");
         }
         int max=0;
-        for(int heigh: heights){
-            max=Math.max(max,heigh);
-            for(int i=max;i>0;i--){
+        for(int heigh: heights) {
+            max = Math.max(max, heigh);
+        }
+        for(int i=max;i>0;i--){
+            for(int heigh:heights){
                 if(heigh>=i){
                     System.out.println("||");
                 }else{
                     System.out.println(" ");
                 }
             }
+            System.out.println();
         }
+        for(int i=0; i<heights.length; i++){
+            System.out.println("...");
+        }
+        System.out.println();
         for(int heigh:heights){
             System.out.printf("%2d ",heigh);
         }
-        System.out.println("");
+        System.out.println();
     }
     public static void displayResult(int[] heights, int maxArea){
         System.out.println("the max area of rectangle is: "+maxArea);
