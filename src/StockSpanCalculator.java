b8ce217bf2552f33bbda796ffa8aa3bf8e8c8eda
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-28 16:29:41 +0400

    add Task 20 - Calculate Stock Span Using Java Stack

--- src/StockSpanCalculator.java
+++ src/StockSpanCalculator.java
@@ -1,8 +1,18 @@
+import java.util.Stack;
+
 public class StockSpanCalculator {
-    public static void main(String[] args){}
+    static Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args){
+        int[][] test={{100, 80, 60, 70, 60, 75, 85},{10, 20, 30, 40, 50},{50, 40, 30, 20, 10},{},{70}};
+
+    }
     public static int[] calculateSpan(int[] prices){}
     public static int calculateSpanBruteForce(int[] prices){}
-    public static void displayResults(int[] prices, int[] spans){}
+    public static void displayResults(int[] prices, int[] spans){
+        for(int i=0;i<prices.length;i++){
+            System.out.println("");
+        }
+    }
     public static void visualizeSpans(int[] prices, int[] spans){}
 
 }
