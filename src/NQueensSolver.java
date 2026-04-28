cd6e12d6604159d2ba377d469b3241afca91be86
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-28 10:04:07 +0400

    add Task 19 - Solve N-Queens Problem Using Stack-Based Backtracking

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -1,6 +1,11 @@
+import javax.swing.*;
+import javax.swing.text.Position;
+import java.util.Stack;
+
 public class NQueensSolver {
+    static Stack<Integer> stack = new Stack<>();
     public static void main(String[] args){}
-    public static int solveNQueens(int n){}
+    public static Position solveNQueens(int n){}
     public static boolean isSafe(Position pos, Stack<Position> queens){}
     public static void displayBoard(Stack<Position> queens, int n){}
     public static void displayAllSolutions(int n){}
