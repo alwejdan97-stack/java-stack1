0947907709773d721e5ac828e858bc249a3e5fd3
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-28 08:40:29 +0400

    add Task 18 - Find Largest Rectangle in Histogram Using Stack

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -1,3 +1,7 @@
 public class LargestRectangleInHistogram {
     public static void main(String[] args){}
+    public static int findLargestRectangle(int[] heights){}
+    public static int calculateArea(int[] heights, int start, int end, int height){}
+    public static int displayHistogram(int[] heights){}
+    public static int displayResult(int[] heights, int maxArea){}
 }
