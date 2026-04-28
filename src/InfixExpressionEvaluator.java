7c72a35c8683b58b10e07c4fe8e5093bfba903a2
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-28 09:05:48 +0400

    add Task 17 - Evaluate Infix Expression Directly Using Two Stacks

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -1,6 +1,8 @@
 import java.util.Stack;
 
 public class InfixExpressionEvaluator {
+    static Stack<Double> operand = new Stack<>();
+    static Stack<Character> operator = new Stack<>();
     public static void main(String[] args){
         String[] expression={"3+4*2","(1+2)*3","10+5*6","100*(2+12)","8+2*5-3","(7+3)*(5-2)","20/5+6*3", "2^3+4", "10%3+6", "5+6*(2+3)", "50/(5+5)", "9-3*2"};
         for(String str:expression){
@@ -8,36 +10,49 @@ public class InfixExpressionEvaluator {
             System.out.println("result: "+evaluate(str));
         }
     }
-    public static double evaluate(String expression) { //Main evaluation method
-        Stack<String> operand = new Stack<>();
-        Stack<String> operator = new Stack<>();
+    public static String evaluate(String expression) { //Main evaluation method
         for (int i = 0; i < expression.length(); i++) {
             char chr = expression.charAt(i);
+            double hr = Character.getNumericValue(chr);
             if (chr == ' ') {
                 continue;
             }
-            if(isNumber(chr)) {
+            if (isNumber(chr)) {
                 StringBuilder number = new StringBuilder();
                 if (i < expression.length() && isNumber(expression.charAt(i))) {
-                    number.append(chr);
+                    number.append(expression.charAt(i));
                 }
-                //operand.push(Double.parseDouble(number.toString()));
-            }
-            if(chr=='('){
+                operand.push(Double.parseDouble(number.toString()));
+            }else if(chr == '('){
                 operator.push(chr);
-            }else if(chr==')'){
-                while(!(operator.isEmpty())&& operator.peek()!='('){
-                    operator.pop();
+            }else if (chr == ')'){
+                while (!(operator.isEmpty()) && operator.peek() != '(') {
+                    double a=operand.pop();
+                    double b=operand.pop();
+                    char op=operator.pop();
+                    operand.push(applyOperation(a,b,op));
                 }
-            }else if(isOperator(chr)){
-                while(!(operator.isEmpty())&&operator.peek()!='(' && getPrecedence(operator.peek())>=getPrecedence(chr)){}
-                    operator.push(chr);
+                //operator.pop(); //remove (
+            } else if (isOperator(chr)) {
+                while (!(operator.isEmpty()) && operator.peek() != '(' && getPrecedence(operator.peek()) >= getPrecedence(chr)) {
+                    double a=operand.pop();
+                    double b=operand.pop();
+                    char op=operator.pop();
+                    operand.push(applyOperation(a,b,op));// apply the operation
+                }
+                //operator.push(chr);// remove (
             }
-            while(!(operator.isEmpty())){
-                operand.pop();
+            while (!(operator.isEmpty())){
+                double a=operand.pop();
+                double b=operand.pop();
+                char op=operator.pop();
+                operand.push(applyOperation(a,b,op));
             }
+            //operator.pop();
         }
+        return String.valueOf(operand.pop());
     }
+
     public static double applyOperation(double a, double b, char operator){//Perform arithmetic
         if(operator=='+'){
             return a+b;
