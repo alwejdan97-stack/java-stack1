0d5e005fcd2226081ec610b4a8ef9288b63dfa95
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-29 10:41:43 +0400

    add Task 3 - visitting website and return the current website and the history, as will as removing website and check the history

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -1,23 +1,29 @@
-import java.util.Stack;
 
+import java.util.*;
 public class BrowserHistorySimulator {
     public static void main(String[] args) {
-        Stack<String> browserHistorySimulator = new Stack<>();// create stack
-        browserHistorySimulator.push("https://www.github.com"); // add element to the stack
-        browserHistorySimulator.push("https://www.google.com");
-        browserHistorySimulator.push("https://www.youtube.com");
-        browserHistorySimulator.push("https://www.Linkedin.com");
-        browserHistorySimulator.push("https://www.Instgram.com");
-        System.out.println("the current page and all pages in history after each visit is:" +browserHistorySimulator);
-        System.out.println("Back 2 ");//using pop to back
-        System.out.println(browserHistorySimulator.pop());
-        System.out.println(browserHistorySimulator.pop());
-        System.out.println("Display the current page and all pages in history after each visit :" +browserHistorySimulator); // Displayed element after pop
-//peek the to element
-        System.out.println("peek the top URL:" +browserHistorySimulator);
-        System.out.println("check if the stack empty or not: "+ browserHistorySimulator.isEmpty()); // Check if the history is empty at different points
-        browserHistorySimulator.push("https://www.anthrophic.com");//added 2 elements
-        browserHistorySimulator.push("https://www.z.ai");
-        System.out.println(browserHistorySimulator); //display all element
-        }
+        Stack<String> web = new Stack<>(); // create the web stack
+        web.push("www.youtube.com"); // add element to the stack
+        System.out.println("the current website is: " + web.peek()); // return the last website (current web page)
+        System.out.println(web); // print the full ftack
+        web.push("www.google.com");
+        System.out.println("the current website is: " + web.peek());
+        System.out.println(web);
+        web.push("www.drive.com");
+        System.out.println("the current website is: " + web.peek());
+        System.out.println(web);
+        web.push("www.github.com");
+        System.out.println("the current website is: " + web.peek());
+        System.out.println(web);
+        web.push("www.linkedin.com");
+        System.out.println("the current website is: " + web.peek());
+        System.out.println(web);
+        web.pop(); // remove the last added web for the first time
+        web.pop(); // remove the last added web for the second time
+        System.out.println("the last website after removing 2 last website is: "+web.peek());
+        System.out.println("check if the stack have websites or not: "+web.isEmpty()); // check if the stack empty or not (return false)
+        web.push("www.snapchat.com");
+        web.push("www.inastagram.com");
+        System.out.println("final stack after visiting 2 websites is: "+web);
+    }
 }
